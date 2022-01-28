4fbcbe09067a8d22b12c3419ef690dcbe39e836e
RedstoneBoy0509 <jacob.rutledge0509@gmail.com>
2022-01-27 20:37:34 -0600

    Initial commit

---
+++ src/main/java/io/github/red050911/plugin/combatlogsecurity/CombatLogSecurity.java
@@ -0,0 +1,17 @@
+package io.github.red050911.plugin.combatlogsecurity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatLogSecurity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
